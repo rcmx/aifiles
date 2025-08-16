71ea42de7f9d7cf375533fa11175b17bd97ac301
rcmx <4450944+rcmx@users.noreply.github.com>
2025-08-16 10:57:43 -0700

    refactor/cleanup

--- installLinux.sh
+++ installLinux.sh
@@ -12,8 +12,8 @@ fi
 
 # claude code 
 cp -r .claude/ ~/
-claude mcp add postgres docker run -i --rm -e POSTGRES_URL mcp/postgres ${POSTGRES_URL}
 claude mcp add --scope user context7 npx @upstash/context7-mcp
 claude mcp add --scope user playwright npx @playwright/mcp@latest
+claude mcp add --scope user postgres docker run -i --rm -e POSTGRES_URL mcp/postgres ${POSTGRES_URL}
 claude mcp add --scope user --transport http microsoft_docs_mcp https://learn.microsoft.com/api/mcp
 
