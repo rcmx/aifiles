fc13316231201c157e3463d37d75cd20260bc721
rcmx <4450944+rcmx@users.noreply.github.com>
2025-08-16 10:57:43 -0700

    refactor/cleanup

--- installLinux.sh
+++ installLinux.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
+
+# vscode
+mkdir -p ~/.config/Code/User
+cp mcp-msft.json ~/.config/Code/User/mcp.json # vscode
+
+# cursor
+if [ -d ~/.cursor ]; then
+    cp mcp-msft.json ~/.cursor/mcp.json
+fi
+
+# claude code 
 cp -r .claude/ ~/
+claude mcp add postgres docker run -i --rm -e POSTGRES_URL mcp/postgres ${POSTGRES_URL}
+claude mcp add --scope user context7 npx @upstash/context7-mcp
+claude mcp add --scope user playwright npx @playwright/mcp@latest
+claude mcp add --scope user --transport http microsoft_docs_mcp https://learn.microsoft.com/api/mcp
 
--- installWin.ps1
+++ installWin.ps1
@@ -1,8 +1,16 @@
-cp .claude ~/ -recurse -force
 
-# mcp's
-cp mcp.json $env:appdata/code/user/ # vscode
-cp mcp.json ~/.cursor/              # cursor
-cp mcp.json ~/.mcp.json             # NOT SUPPORTED
+# vs and vscode
+cp mcp-msft.json $env:appdata/code/user/mcp.json # vscode
+cp mcp-msft.json ~/.mcp.json # visual studio
+
+# cursor
+if (Test-Path ~/.cursor) { cp mcp-msft.json ~/.cursor/mcp.json }     # cursor
+
+# claude code 
+cp .claude ~/ -recurse -force
+claude mcp add --scope user context7 npx @upstash/context7-mcp
+claude mcp add --scope user playwright npx @playwright/mcp@latest
+claude mcp add --scope user postgres docker 'run -i --rm -e POSTGRES_URL mcp/postgres ${env:POSTGRES_URL}'
+claude mcp add --scope user --transport http microsoft_docs_mcp https://learn.microsoft.com/api/mcp
 
 
